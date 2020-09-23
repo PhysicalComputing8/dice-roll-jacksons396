9730e9c399defc32c662391ecde512fbfd2ddcc7
jacksons396 <71662803+jacksons396@users.noreply.github.com>
2020-09-23 09:46:10 -0400

    added dares

--- main.ts
+++ main.ts
@@ -3,57 +3,21 @@ input.onGesture(Gesture.Shake, function () {
     //  I have the program generate a random number between 1 and 6, and I assign that value to the roll variable
     roll = randint(1, 6)
     if (roll == 1) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
+        basic.showString("Pour Acid On Your Head")
     } else {
         if (roll == 2) {
-            basic.showLeds(`
-                . . . . .
-                . # . . .
-                . . . . .
-                . . . # .
-                . . . . .
-                `)
+            basic.showString("Put Your Tongue On The Hot Stove")
         } else {
             if (roll == 3) {
-                basic.showLeds(`
-                    # . . . .
-                    . . . . .
-                    . . # . .
-                    . . . . .
-                    . . . . #
-                    `)
+                basic.showString("Pull Out Your Front Teeth")
             } else {
                 if (roll == 4) {
-                    basic.showLeds(`
-                        # . . . #
-                        . . . . .
-                        . . . . .
-                        . . . . .
-                        # . . . #
-                        `)
+                    basic.showString("Kill Your Best Friend")
                 } else {
                     if (roll == 5) {
-                        basic.showLeds(`
-                            # . . . #
-                            . . . . .
-                            . . # . .
-                            . . . . .
-                            # . . . #
-                            `)
+                        basic.showString("Deep Fry Your Fingers And Eat Them")
                     } else {
-                        basic.showLeds(`
-                            # . . . #
-                            . . . . .
-                            # . . . #
-                            . . . . .
-                            # . . . #
-                            `)
+                        basic.showString("Cut Off Two Toes")
                     }
                 }
             }
@@ -62,4 +26,4 @@ input.onGesture(Gesture.Shake, function () {
 })
 // Code added here will run once when the program starts
 let roll = 0
-basic.showString("Shake to roll the dice")
+basic.showString("I DARE you to play...")
